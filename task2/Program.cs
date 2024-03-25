13c87dff252145b9501ccfcd45e3e06ddef32c82
Artem <kos107@bk.ru>
2024-03-25 22:29:20 +0300

    код удаляет вторую цифру из трехзначногочисла

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и удаляет вторую цифру этого числа.
+
+using System.Security.AccessControl;
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+{
+    //Первый вариант
+    int left = num / 100;
+    int right = num % 10;
+    //Второй вориант
+    int newnum = left * 10 + right;
+    System.Console.WriteLine(newnum);
+    //Первый способ
+    //System.Console.WriteLine(left + "" + rigt);
+    //Второй способ
+    // System.Console.WriteLine($"{left}{right}");
+}
+else
+{
+    System.Console.WriteLine("вы ввели не трехзначное число!");
+}
