ed11d1c66dc0253572a4aef700ecf8b5feb2735a
Artem <kos107@bk.ru>
2024-03-25 13:12:39 +0300

    комманда на четное или не четное число

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿//Напиши программу,которая на вход принимает число и выдает,являится ли число четным 
+
+System.Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    System.Console.WriteLine("Чутное");
+}
+else
+{
+    System.Console.WriteLine("не четное");
+}
