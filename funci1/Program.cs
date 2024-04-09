0aaee916defee4f88721a6188b7ce29408a7a118
Artem <kos107@bk.ru>
2024-04-09 13:05:59 +0300

    Лекция

--- funci1/Program.cs
+++ funci1/Program.cs
@@ -12,16 +12,45 @@
 
 //-----------------------------------------------------------------------------------------------------------------------
 //Функция нахождения квадрата 
-void PrintSquares(int limit)
-{
-    int i = 1;
-    while (i <= limit)
-    {
-        Console.Write($"{i*i} ");
-        i++;
-    }
-}
+// void PrintSquares(int limit)
+// {
+//     int i = 5;
+//     while (i <= limit)
+//     {
+//         Console.Write($"{i*i} ");
+//         i++;
+//     }
+// }
 
-PrintSquares(5);
-PrintSquares(10);
-PrintSquares(15);
+// PrintSquares(5);
+// PrintSquares(10);
+// PrintSquares(15);
+//----------------------------------------------------------------------------------------------------------------------
+//Функция которая обнуляет четные цифры массива 
+
+// void ZeroEvenElements(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] % 2 == 0)
+//         {
+//             arr[i] = 0;
+//         }
+//     }
+// }
+// void PrintArray(int[] arr)
+// {
+//     foreach (int e in arr)
+//     {
+//         System.Console.Write($"{e} ");
+//     }
+// }
+// int[] array = { 1, 2, 3, 4, 5 };
+// ZeroEvenElements(array);
+// PrintArray(array);
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+
+
