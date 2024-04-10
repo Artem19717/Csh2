5937e0e8b00e7b2d5b689fef1f4656cac4609de4
Artem <kos107@bk.ru>
2024-04-10 20:28:40 +0300

    Двумкрный масив

---
+++ massiw2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Заполнение массива случайными числами от 1 до 10
+
+
+int[,] createMatrix(int powCount, int columsCount)
+{
+    int[,] matrix = new int[powCount, columsCount];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] matrix = createMatrix(4, 5);
+ShowMatrix(matrix);
